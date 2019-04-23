71bcb0b70538148c08dca929767913f553bd698d
Travis CI <travis@travis-ci.org>
2019-04-23 15:38:39 +0300

    WIP

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,9 @@ cp -r dist docs/
 cp -r styles docs/
 cp -r assets docs/
 
+ git checkout -b gh-pages
 git status
-git add --all
+git add . --all
 git commit -am "Travis build: $TRAVIS_BUILD_NUMBER"
-git push
+git remote add origin-pages https://${GH_TOKEN}@github.com/babkamen/breakout-game.git > /dev/null 2>&1
+git push --quiet --set-upstream origin-pages gh-pages 
