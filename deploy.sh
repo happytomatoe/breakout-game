d23d39d53754f6de0a4c7a24b5379f13a67d7a46
babkamen <babkamen@gmail.com>
2019-04-23 15:18:01 +0300

    WIP

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+
+
 cp index.html docs/
 cp -r dist docs/
 cp -r styles docs/
 cp -r assets docs/
+
+ git add --all
+git commit -am "Travis build: $TRAVIS_BUILD_NUMBER"
+git push
